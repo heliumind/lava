9aa410705b874883bd3579775efe4d44b908407c
Henry He <henryhe98.c@gmail.com>
2021-04-17 02:31:29 +0200

    Add Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,36 @@
+import React from 'react';
 import logo from '../../assets/logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Header from '../Header';
+
+const useStyles = makeStyles({
+  mainContainer: {
+    paddingTop: '20px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="column">
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid item container spacing={2} className={classes.mainContainer}>
+        <Grid item xs={8}>
+          <Typography variant="h6" gutterBottom>
+            Text input
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          Instagram stroy preview
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
