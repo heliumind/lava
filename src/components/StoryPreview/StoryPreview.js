4f7a3b6c4c89a8a02f112940e7942e9dd1c7cbe6
samuelvontucher <work@samuelvontucher.com>
2021-04-17 21:41:39 +0200

    Added InstaStory Component and changed StoryPreview Component

--- src/components/StoryPreview/StoryPreview.js
+++ src/components/StoryPreview/StoryPreview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
+import InstaStory from '../InstaStory';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -75,58 +76,7 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '1em',
     color: '#aaaaaa',
   },
-  storyImg: {
-    position: 'relative',
-    height: '55%',
-    width: '100%',
-    backgroundImage:
-      'url("https://img.br.de/66e5975b-3efc-49c8-bbd7-9522b0658815.jpeg?q=80&rect=8%2C174%2C5255%2C2958&w=2000")',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'right',
-  },
-  hashtagsWrapper: {
-    position: 'absolute',
-    bottom: '0px',
-    width: '100%',
-    minHeight: '20%',
-    padding: '0.5em 3em',
-    display: 'flex',
-    alignItems: 'flex-end',
-    background: 'rgb(0,0,0)',
-    background:
-      'linear-gradient(0deg, rgba(0,0,0,0.798739564185049) 0%, rgba(0,0,0,0) 100%)',
-    color: '#fff',
-    fontSize: '1.2em',
-  },
-  hashtag: {
-    marginRight: '1.2em',
-  },
-  storyContent: {
-    height: '45%',
-    width: '100%',
-    padding: '3em',
-    backgroundColor: '#FFF',
-    textAlign: 'center',
-  },
-  storyTitleWrapper: {
-    fontSize: '2em',
-    fontWeight: 'bold',
-    backgroundColor: '#222',
-  },
-  storyTitle: {
-    color: '#fff',
-  },
-  storyText: {
-    textAlign: 'left',
-    fontSize: '1.5em',
-  },
 }));
-
-function getNthHashtag(hashtags, index) {
-  hashtags = hashtags.split(', ');
-  return hashtags.length > index ? hashtags[index] : '';
-}
 
 function StoryPreview(props) {
   const classes = useStyles();
@@ -148,31 +98,8 @@ function StoryPreview(props) {
           </div>
         </div>
         <div className={classes.gradient}></div>
-        <div className={classes.storyImg}>
-          <div className={classes.hashtagsWrapper}>
-            <div>
-              <span className={classes.hashtag}>
-                {getNthHashtag(props.storyState.Hashtag, 0)}
-              </span>
-              <span className={classes.hashtag}>
-                {getNthHashtag(props.storyState.Hashtag, 1)}
-              </span>
-              <span className={classes.hashtag}>
-                {getNthHashtag(props.storyState.Hashtag, 2)}
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className={classes.storyContent}>
-          <div className={classes.storyTitleWrapper}>
-            <div className={classes.storyTitle}>
-              {props.storyState.Schlagzeile}
-            </div>
-          </div>
-          <div className={classes.storyText}>
-            <p>{props.storyState.Zusammenfassung}</p>
-          </div>
-        </div>
+        <InstaStory storyState={props.storyState}></InstaStory>
+        <InstaStory storyState={props.storyState}></InstaStory>
       </div>
     </div>
   );
