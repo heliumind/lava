fb7f94fffbcbb0ee6d55b22729a25f7a248788e3
Henry He <henryhe98.c@gmail.com>
2021-04-17 15:19:23 +0200

    Update NewsInput component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,9 +8,6 @@ import Header from '../Header';
 import NewsInput from '../NewsInput';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
   image: {
     backgroundImage: 'url(https://source.unsplash.com/random)',
     backgroundRepeat: 'no-repeat',
@@ -21,27 +18,10 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
   root: {
     paddingTop: '20px',
-    paddingLeft: '80px',
-    paddingRight: '80px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
     height: '100vh',
   },
 }));
--- src/components/NewsInput/NewsInput.js
+++ src/components/NewsInput/NewsInput.js
@@ -1,27 +1,19 @@
 import React from 'react';
+import { Formik, Field, Form } from 'formik';
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    margin: theme.spacing(8, 4),
+    margin: theme.spacing(4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -37,61 +29,80 @@ function NewsInput() {
 
   return (
     <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Sign in
-      </Typography>
-      <form className={classes.form} noValidate>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-        >
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item xs>
-            <Link href="#" variant="body2">
-              Forgot password?
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link href="#" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
+      <Formik
+        initialValues={{
+          articleURL: '',
+          articleText: '',
+        }}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          // make async call
+          console.log('submit: ', data);
+          setSubmitting(false);
+        }}
+      >
+        {({
+          values,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          resetForm,
+        }) => (
+          <Form className={classes.form}>
+            <Typography variant="h6">Article Input</Typography>
+            <Field
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="url"
+              label="Article URL"
+              name="articleURL"
+              type="url"
+              as={TextField}
+            />
+            <Typography variant="body">or</Typography>
+            <Field
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              multiline="true"
+              rows={6}
+              name="articleText"
+              label="Article Text"
+              id="article"
+              autoFocus
+              as={TextField}
+            />
+            <Grid container spacing={2}>
+              <Grid item>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  disabled={isSubmitting}
+                >
+                  Generate
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  type="reset"
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={resetForm}
+                >
+                  Clear
+                </Button>
+              </Grid>
+            </Grid>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
