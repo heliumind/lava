37f8ddc25d1eb2ab3afcf98db4e64dc48adc64fa
Henry He <henryhe98.c@gmail.com>
2021-04-17 12:02:48 +0200

    Add left right view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,34 +3,71 @@ import logo from '../../assets/logo.svg';
 import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Header from '../Header';
+import NewsInput from '../NewsInput';
 
-const useStyles = makeStyles({
-  mainContainer: {
-    paddingTop: '20px',
-    paddingLeft: '50px',
-    paddingRight: '50px',
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
   },
-});
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  root: {
+    paddingTop: '20px',
+    paddingLeft: '80px',
+    paddingRight: '80px',
+    height: '100vh',
+  },
+}));
 
 function App() {
   const classes = useStyles();
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Header />
-      </Grid>
-      <Grid item container spacing={2} className={classes.mainContainer}>
-        <Grid item xs={8}>
-          <Typography variant="h6" gutterBottom>
-            Text input
-          </Typography>
+    <div>
+      <Header />
+      <Grid container component="main" className={classes.root}>
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          component={Paper}
+          elevation={6}
+          square
+        >
+          <NewsInput />
         </Grid>
-        <Grid item xs={4}>
-          Instagram stroy preview
+        <Grid item xs={12} sm={8} md={5} className={classes.image}>
+          Story preview
         </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
 
